9f83fc2b6836a4232a1a0559bd5fbaed3edfd8ce
Brent Coney <brentconey@gmail.com>
2015-09-07 21:10:32 -0500

    Worked on added a new staff member and starring the employee of the month.

--- www/bindings.js
+++ www/bindings.js
@@ -20,9 +20,7 @@ ko.bindingHandlers.inlineEdit = {
     init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
         var value = valueAccessor();
         $(element).text(ko.unwrap(value));
-        $(element).on("input", function (e) {
-            value($(this).text());
-        });
+        
 		$(element).on("focus click", function (e) {
 			var sel, range;
 			if (window.getSelection && document.createRange) {
@@ -41,6 +39,60 @@ ko.bindingHandlers.inlineEdit = {
 	update: function (element, valueAccessor) {
 		if ($(element).text() !== ko.unwrap(valueAccessor())) {
 			$(element).text(ko.unwrap(valueAccessor()));
+            console.log("The text is: " + $(element).text());
+            console.log("The value accessor is: " + ko.unwrap(valueAccessor()));
 		}
+        $(element).on("input", function (e) {
+            valueAccessor($(this).text());
+            //it's loosing the binding context because value is the first on init
+        });
+        //$(element).text(ko.unwrap(valueAccessor()));
 	}
+};
+
+function toJSON(rootObject, replacer, spacer) {
+    var cache = [];
+    var plainJavaScriptObject = ko.toJS(rootObject);
+    var replacerFunction = replacer || cycleReplacer;
+    var output = ko.utils.stringifyJson(plainJavaScriptObject, replacerFunction, spacer || 2);
+    cache = null; 
+    return output;
+
+    function cycleReplacer(key, value) {
+        if (['entityAspect', 'entityType', '_$typeName'].indexOf(key) !== -1) {
+            return;
+        }
+        if (typeof value === 'object' && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+                return; 
+            }
+            cache.push(value);
+        }
+        return value;
+    }
+}
+
+ko.bindingHandlers.dump = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewmodel, bindingContext) {
+        var context = valueAccessor();
+        var allBindings = allBindingsAccessor();
+        var pre = document.createElement('pre');
+
+        element.appendChild(pre);
+
+        var dumpJSON = ko.computed({
+            read: function () {
+                var en = allBindings.enable === undefined || allBindings.enable;
+                return en ? toJSON(context, null, 2) : '';
+            },
+            disposeWhenNodeIsRemoved: element
+        });
+
+        ko.applyBindingsToNode(pre, {
+            text: dumpJSON,
+            visible: dumpJSON
+        });
+
+        return { controlsDescendentBindings: true };
+    }
 };
